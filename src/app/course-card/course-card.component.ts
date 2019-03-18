b496a8d84a8f2d5d6037deb1d7a65769c3eb46cd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-18 12:36:48 -0700

    working on the card comp

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -8,6 +8,7 @@ import { Course } from '../model/course';
 })
 export class CourseCardComponent implements OnInit {
   @Input() course: Course;
+  @Input() cardIndex: number;
 
   @Output() courseSelected = new EventEmitter<Course>();
 
@@ -16,9 +17,23 @@ export class CourseCardComponent implements OnInit {
   ngOnInit() {
   }
 
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+
   onCourseViewed() {
     console.log('CLICKED!');
     this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    return this.course.category === 'BEGINNER' ? 'beginner' : '';
+  }
+
+  cardStyles() {
+    return {
+      'background-image': `url(${this.course.iconUrl})`,
+    };
   }
 
 }
