83860e14485030516ecf6e0a58b0857efad970b1
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-19 08:33:19 -0700

    viewChild & ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,58 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CachedResourceLoader } from '@angular/platform-browser-dynamic/src/resource_loader/resource_loader_cache';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  @ViewChild('cardRef1') card1: CourseCardComponent;
-  @ViewChild('cardRef2', { read: ElementRef }) card2: CourseCardComponent;
+  // @ViewChild('cardRef1') card1: CourseCardComponent;
+  // @ViewChild('cardRef2', { read: ElementRef }) card2: CourseCardComponent;
 
-  @ViewChild('container') container: ElementRef;
+  // @ViewChild('container') container: ElementRef;
+
+  @ViewChildren(CourseCardComponent, { read: ElementRef }) cards: QueryList<CourseCardComponent>;
 
   courses = COURSES;
 
-  title = 'this is a title';
+  // title = 'this is a title';
 
-  startDate = new Date(2000, 0, 1);
+  // startDate = new Date(2000, 0, 1);
 
-  price = 9.92335333;
+  // price = 9.92335333;
 
-  rate = 0.67;
+  // rate = 0.67;
 
-  course = COURSES[0];
+  // course = COURSES[0];
+
+  ngAfterViewInit() {
+    console.log(this.cards);
+    this.cards.changes.subscribe(card => {
+      console.log(card);
+    });
+    // console.log('Card!!', this.cards.first);
+  }
 
   onCourseSelected(course: Course) {
-    // console.log('Selected!!', course);
-    console.log('Card!!', this.card1);
-    console.log('Card!!', this.card2);
+    console.log('Selected!!', course);
+    // console.log('Card!!', this.card1);
+    // console.log('Card!!', this.card2);
     // console.log('Container!!', this.container);
+  }
+
+  onCoursesEdited() {
+    this.courses.push({
+      id: 1,
+      description: 'Hello',
+      iconURL: 'none',
+      longDescription: 'Hellooooo',
+      lessonsCount: 1000
+    });
   }
 
 }
