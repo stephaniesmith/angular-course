f3e2f0087e7915f2cc13d2743140356b5f62531e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-19 08:03:17 -0700

    learning about pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = 'this is a title';
+
+  startDate = new Date(2000, 0, 1);
+
+  price = 9.92335333;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course: Course) {
     console.log('Selected!!', course);
   }
