eddf5728bd963526b52bc8ad7efd400c22953479
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-22 07:40:33 -0700

    more work with directives

--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[ngxUnless]'
 })
 export class NgxUnlessDirective {
 
-  constructor() { }
+  visible = false;
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) { }
+
+  @Input() set ngxUnless(condition: boolean) {
+    if (!condition && !this.visible) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+  }
 
 }
