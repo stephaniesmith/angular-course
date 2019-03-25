5d3b33297c17c373fcd58de5ce37a33160145b50
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-25 07:57:18 -0700

    http and async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,18 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$: Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+    const params = new HttpParams()
+      .set('page', '1')
+      .set('pageSize', '10');
+
+    this.courses$ = this.http.get<Course[]>('/api/courses', { params });
   }
 
 
