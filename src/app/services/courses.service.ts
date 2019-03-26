413616e14541aa98240efd02cfdd1bed55abb77e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-26 07:39:30 -0700

    building a course service

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private http: HttpClient) { }
+
+  loadCourses(): Observable<Course[]> {
+    const params = new HttpParams()
+      .set('page', '1')
+      .set('pageSize', '10');
+
+    return this.http.get<Course[]>('/api/courses', { params });
+  }
+}
